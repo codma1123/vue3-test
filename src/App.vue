acfba33791905e2face08b2aa470b550f8b0eef3
codma <ruddud1123@naver.com>
2022-03-05 18:25:04 +0900

    pinia init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h2>Hello Pinia!</h2>
+    <div>
+      <h4>{{ counter.count }}</h4>
+      <h4>Doubled {{ counter.doubleCount }}</h4>
+      <button type="button" @click="counter.increment">increment</button>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import { useCounterStore } from "@/stores/counter";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+  name: "App",
+  setup() {
+    const counter = useCounterStore();
+
+    return {
+      counter,
+    };
+  },
+});
 </script>
 
 <style>
---
+++ src/components/PiniaTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    counter : {{ counter }} 
+  </div>
+  <div>
+    <button @click="increaseCounter">
+      click!
+    </button>
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from '@/stores/counter'
+
+export default {
+  setup() {
+    const counterStore = useCounterStore()
+
+    return { counterStore }
+  },
+  computed: {
+    
+  },
+  methods: {
+    increaseCounter() {
+      console.log('ss')
+      this.counterStore.increment()
+      console.log('new count:', this.counterStore.count)
+    }
+  }
+}
+</script>
